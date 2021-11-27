d4dcdd743e02d26e04499ad808ef151f30623084
kimyuminnn <k3404533@naver.com>
2021-11-27 15:57:09 +0900

    go to home real

--- go to home.c
+++ go to home.c
@@ -1,15 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-int menu();
-void search_bus(int list[], int n, int key);
-int get_trans_time(void);
+
+int menu() {   //셔틀(상행)버스 메뉴 선택 출력 함수
+	printf("\n================ < 하 행 버 스 > =================");
+	printf("\n\n                 (학교 -> 신창역)\n\n");
+	printf("\n\n  1. 입력된 시간에서 가장 가까운 버스 시간 보기\n\n");
+	printf("  2. 버스 전체 시간표 출력하기 \n\n");
+	printf("  3. 종료 \n\n\n");
+	printf("==================================================\n\n");
+	printf("       원하는 서비스의 번호를 입력해 주세요\n\n ");
+	printf("                  -> ");
+	int choice;
+	scanf("%d", &choice);
+	return choice;
+}
+int get_trans_time(void) {    //시간과 분을 입력받고 시간에 *60을 하고 분을 더해 반환하는 함수
+	printf("\n\n\n      시간을 입력하세요 (시 분) : ");
+	int h, m, key;
+	scanf("%d %d", &h, &m);  //시간과 분을 띄어쓰기로 구분
+	return key = h * 60 + m;
+}
+void search_bus(int list[], int n, int key)  //가장 가까운 버스가 몇 번째 배열인지 순차탐색하는 함수
+{
+	int i = 0;
+	int o_key = key; // 원본 키를 o_key로 복사한다
+	while (list[i] != key)
+	{
+		for (i = 0; i < n; i++) { //n(SIZE)까지 배열을 비교
+			if (key == list[i]) { //키랑 list[i]가 같으면 출력 후 종료
+				printf("\n\n        가장 가까운 버스는 %d시 %d분입니다\n\n", list[i] / 60, list[i] % 60);
+				printf("           대기 시간은 %d시간 %d분입니다\n\n\n\n", (key - o_key) / 60, (key - o_key) % 60);
+				return 0;
+			}
+		}
+		key = key + 1; //가장 가까운 list[i]를 못찾으면 key에 1을 합함
+		if (key > 14400) { //키가 24*60을 넘어가면 시간 오류 출력 후 종료
+			printf("시간 오류!");
+			return 0;
+		}
+	}
+}
 int main(void)
 {
-	FILE *fp = NULL;
+	FILE* fp = NULL;
 	int list[10000] = { 0 };
-	system("mode con:cols=50 lines=40");   //콘솔창 크기 조절
-
 	while (1)
 	{
 		fp = fopen("buss.txt", "r");
@@ -19,66 +54,31 @@ int main(void)
 			fscanf(fp, "%d", &list[save_size]);
 		}
 		int SIZE = save_size - 1; //save_size는 0이 추가되기때문에 
-		fclose(fp);               //실제 저장된 정수의 수는 save_size에서 1을 뺀다
+		fclose(fp);               //실제 저장된 정수의 수는 save_size에서 1을 뺀 수
 
 		switch (menu())
 		{
 		case 1:   //시간을 입력하고 가장 가까운 버스시간 찾기
-			printf("버스 시간 찾기\n");
+			system("cls");
+			printf("\n================ < 하 행 버 스 > =================");
+			printf("\n\n       가장 가까운 버스의 시간을 출력합니다\n");
 			int h, m, key;
-			key = get_trans_time();  // 이 함수는 시간과 분을 입력받고 시간에 *60을 하고 분을 더해 반환하는 함수
-			search_bus(list, SIZE, key);  // 이 함수는 가장 가까운 버스가 몇번째 배열인지 순차탐색하는 함수
+			key = get_trans_time();  //시간과 분을 입력받고 시간에 *60을 하고 분을 더해 반환하는 함수
+			search_bus(list, SIZE, key);  //가장 가까운 버스가 몇 번째 배열인지 순차탐색하는 함수
 			break;
 		case 2:
-			fp = fopen("buss.txt", "a+"); //폴더 오픈
+			fp = fopen("buss.txt", "r"); //폴더 오픈
 			for (int i = 0; i < SIZE; i++)
 			{
-				printf("%d시 %d 분\n", list[i] / 60, list[i] % 60); //리스트를 사이즈만큼 출력
+				printf("%d시 %d분\n", list[i] / 60, list[i] % 60); //리스트를 사이즈만큼 출력
 			}
 			fclose(fp); //폴더닫기
 			break;
-		case 4:
+		case 3:
 			exit(1);
 			break;
 		default:
 			break;
-		}
-	}
-}
-int menu() {
-	printf("=============== < 하 행 버 스 > ===============\n\n");
-	printf(" 1 입력된 시간에서 가장 가까운 버스시간 보기\n");
-	printf(" 2 버스 시간표 출력하기 \n");
-	printf(" 3 종료 \n\n");
-	printf("===============================================\n");
-	printf("선택 : ");
-	int choice;
-	scanf("%d", &choice);
-	return choice;
-}
-int get_trans_time(void) {    //시간과 분을 입력받고 시간에 *60을 하고 분을 더해 반환하는 함수
-	printf("시간을 입력하세요 (시 분) : ");
-	int h, m, key;
-	scanf("%d %d", &h, &m);  //시간과 분을 띄어쓰기 하나로 구분
-	return key = h * 60 + m;
-}
-void search_bus(int list[], int n, int key)  // 가장 가까운 버스가 몇번째 배열인지 순차탐색하는 함수
-{
-	int i = 0;
-	int o_key = key; // 원본 키를 o_key로 복사한다
-	while (list[i] != key)
-	{
-		for (i = 0; i < n; i++) { //n(SIZE) 까지 배열을 비교한다
-			if (key == list[i]) { //키랑 list[i]가 같으면 출력하고 종료
-				printf("가장 가까운 버스는 %d시 %d분입니다\n\n", list[i] / 60, list[i] % 60);
-				printf("대기 시간은 %d시 %d 분입니다\n", (key - o_key) / 60, (key - o_key) % 60);
-				return 0;
-			}
-		}
-		key = key + 1; //가장 가까운 list[i]를 못찾으면 key에 1을 더하기
-		if (key > 14400) { //키가 24*60을 넘어가면 시간 오류 출력 후 종료
-			printf("시간 오류!");
-			return 0;
 		}
 	}
 }
